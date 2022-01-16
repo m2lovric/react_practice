e5e200841af1561c70279f4c955cbc1936c6e8e3
m4lovric <95147089+m4lovric@users.noreply.github.com>
2022-01-16 20:28:30 +0100

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <h1>The Dojo Blog</h1>
+      <section className='links'>
+        <a href='/'>Home</a>
+        <a href='/create'>New Blog</a>
+      </section>
+    </nav>
+  );
+};
+
+export default Navbar;
