d2f07fd5b898a6c0a0db8eb3ff5712bb40046046
m2lovric <m2lovric@gmail.com>
2022-01-16 22:16:05 +0100

    setup Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <section className='content'>
-        <Home />
-      </section>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <section className='content'>
+          <Routes>
+            <Route path='/' element={<Home />} exact />
+            <Route path='/create' element={<Create />} />
+          </Routes>
+        </section>
+      </div>
+    </Router>
   );
 }
 
